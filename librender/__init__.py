f0a4cb7ae705e39c4dce48de91dc17b34c68a800
jercram <jeroen.cramer@hotmail.com>
2024-10-02 09:52:27 +0200

    provide the required .so files and their correct file paths

--- librender/__init__.py
+++ librender/__init__.py
@@ -2,5 +2,5 @@ import ctypes
 import os
 
 pyrender_dir = os.path.dirname(os.path.realpath(__file__))
-ctypes.cdll.LoadLibrary(os.path.join(pyrender_dir, 'pyrender.so'))
+ctypes.cdll.LoadLibrary(os.path.join(pyrender_dir, '../pyrender.so'))
 from pyrender import *
